60f8adde9a2eb31547d472834c6076f25074afe8
Jorge Roldan Barrio <Jorge.RoldanBarrio@zooplus.com>
2020-12-12 14:38:59 +0100

    Create my-books-api submodule as a Spring Boot project

---
+++ my-books-api/README.md
@@ -0,0 +1,3 @@
+This is a Spring Boot project
+
+To run it use: ./gradlew my-books-api:bootRun
---
+++ my-books-api/src/test/java/com/roldan/mybooks/api/MyBooksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roldan.mybooks.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBooksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
