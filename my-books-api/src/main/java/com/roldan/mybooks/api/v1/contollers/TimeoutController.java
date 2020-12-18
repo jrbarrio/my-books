c9ab62dccf6a6fec22ea95d72fb1c3a858f302b7
Jorge Roldan Barrio <Jorge.RoldanBarrio@zooplus.com>
2020-12-18 17:05:22 +0100

    Trying timed out asynchronous execution

---
+++ my-books-api/src/main/java/com/roldan/mybooks/api/v1/contollers/TimeoutController.java
@@ -0,0 +1,20 @@
+package com.roldan.mybooks.api.v1.contollers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Callable;
+
+@RestController
+@RequestMapping(path = "/v1/timeout", produces = "application/json")
+public class TimeoutController {
+
+    @GetMapping
+    public Callable<String> getFoobar() throws InterruptedException {
+        return () -> {
+                Thread.sleep(1000); //this will cause a timeout
+                return "foobar";
+            };
+    }
+}
